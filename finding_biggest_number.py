9ac20ec33ef1bff2ed6a1a55decf03a08be00c2f
maihannn <maicapalillo15@gmail.com>
2024-01-06 19:03:36 +0800

    added input

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -4,5 +4,10 @@
 # Assignment: Ask the user to input 3 numbers. Find and print the biggest number using only if-else statement.
 
 # ask user to input 3 numbers
+name = input ("\033[1;35mInput your name: ")
+number = input("\033[1;35mInput a number: ")
+print (number)
+
+
 # find the biggest number
 # display the result
