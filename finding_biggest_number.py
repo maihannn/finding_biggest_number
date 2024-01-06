07d8afd3eeca5da3f80e090c2b2163b44955b7ba
maihannn <maicapalillo15@gmail.com>
2024-01-06 17:15:01 +0800

    first commit

---
+++ finding_biggest_number.py
@@ -0,0 +1,8 @@
+#Name: Jamaica C. Palillo
+#Section: BSCPE 1-5
+
+# Assignment: Ask the user to input 3 numbers. Find and print the biggest number using only if-else statement.
+
+# ask user to input 3 numbers
+# find the biggest number
+# display the result
