61d4d0110fdd574927211348f8afa46e2e3ae525
maihannn <maicapalillo15@gmail.com>
2024-01-08 14:16:56 +0800

    added if else

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -4,10 +4,17 @@
 # Assignment: Ask the user to input 3 numbers. Find and print the biggest number using only if-else statement.
 
 # ask user to input 3 numbers
-name = input ("\033[1;35mInput your name: ")
-number = input("\033[1;35mInput a number: ")
-print (number)
 
+num1 = (input("Please Enter the First number: "))
+num2 = (input("Please Enter the Second number: "))
+num3 = (input("Please Enter your Third number: "))
+
+#try
+# check if number is the max number
+if (num1 > num2) and (num1 > num3):
+    print ("The maximum number among the three is: " + num1) 
+else:
+    print ("All number are equal.")
 
 # find the biggest number
 # display the result
