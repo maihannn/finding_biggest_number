ed4046359c7ecfe77d27fd3bddf5901e62b1524f
maihannn <maicapalillo15@gmail.com>
2024-01-09 16:25:55 +0800

    added tkinter

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -3,14 +3,21 @@
 
 # Assignment: Ask the user to input 3 numbers. Find and print the biggest number using only if-else statement.
 
-# ask user to input 3 numbers
+# adding window
 
+from tkinter import *
+from tkinter import messagebox
+root = Tk ()
+root.title ("==Finding the Highest Number==")
+root.geometry ( "560x450")
+
+# ask user to input 3 numbers
 # variables
+
 num1 = float ((input ("Please Enter the First Number: ")))
 num2 = float ((input ("Please Enter the Second Number: ")))
 num3 = float ((input ("Please Enter the Third Number: ")))
 maximum = 0
-
 
 #if-else
 if (num1 > num2) and (num1 > num3):
@@ -21,8 +28,3 @@ elif (num2 > num1) and (num2 > num3):
     print ("The highest number among the three is: ", num2)
 else:
     print ("The highest number among the three is: ", num3)
-
-
-# find the biggest number
-    
-# display the result
